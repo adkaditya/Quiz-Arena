abdbf138134cae34a2026816476837d963b7337f
Aditya Kumar <adk6535@gmail.com>
2026-07-30 20:48:03 +0530

    Fix backend URL

--- frontend/src/lib/apiConfig.js
+++ frontend/src/lib/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "https://ai-quiz-backend.onrender.com/api/v1",
+baseURL: "https://ai-role-quiz-generator.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
