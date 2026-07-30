75098dabe603b9f5c720efde848968c80375b24c
Aditya Kumar <adk6535@gmail.com>
2026-07-30 20:16:30 +0530

    Update API URL for production

--- frontend/src/lib/apiConfig.js
+++ frontend/src/lib/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://ai-quiz-backend.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
